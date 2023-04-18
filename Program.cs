825611a1e537c52107f3e1d0ca5de0892b11ba06
togzhan13 <69753372+togzhan13@users.noreply.github.com>
2023-04-18 09:20:02 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A3_TogzhanTolegen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Decalring variables
+            int[,] tempArray;
+            int rows, columns;
+            bool quit = false;
+
+            do
+            {
+                //Calling ArrayData method with return two - dimension array
+                tempArray = ArrayData(out rows, out columns);
+
+                //Writing message for print array data
+                Console.WriteLine("The Original Two Dimension Array Data:");
+                DisplayArray(tempArray, rows, columns);
+
+                Console.WriteLine("\nEnter any key to continue:");  
+                Console.ReadKey();  //Pausing the program           
+
+                //swapRow method
+                SwapRow(tempArray);
+                Console.WriteLine("\nSwap Row method output: ");
+                DisplayArray(tempArray, rows, columns);
+
+                Console.WriteLine("\nEnter any key to continue:");
+                Console.ReadKey();
+
+                //swapColumn method
+                SwapColumn(tempArray);
+                Console.WriteLine("\nSwap Column method output: ");
+                DisplayArray(tempArray, rows, columns);
+
+                Console.WriteLine("\nEnter any key to continue:");
+                Console.ReadKey();
+
+                //RotateLeftDiagonal method
+                RotateLeftDiagonal(tempArray);
+                Console.WriteLine("\nRotateLeftDiagonal method output: ");
+                DisplayArray(tempArray, rows, columns);
+
+                Console.WriteLine("\nEnter any key to continue:");
+                Console.ReadKey();
+
+                //RotateRightDiagonal method
+                RotateRightDiagonal(tempArray);
+                Console.WriteLine("\nRotateRightDiagonal method output: ");
+                DisplayArray(tempArray, rows, columns);
+
+                // Ask the user if they want to quit or continue
+                Console.Write("Do you want to quit? (y/n): ");
+                string input = Console.ReadLine().ToLower();
+                quit = (input == "y" || input == "yes");
+
+            } while (!quit);
+        }
+
+        //Calling ArrayData method with return two-dimension array
+        public static int[,] ArrayData(out int rows, out int columns)
+        {
+            //Promting the user's input
+            Console.WriteLine("Enter the number of rows for the 2D array: ");
+            rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the number of columns for the 2D array: ");
+            columns = Convert.ToInt32(Console.ReadLine());
+
+            //Allocating a new 2D array to the specified size
+            int[,] matrix = new int[rows, columns];
+            Random randNum = new Random();
+
+            //Generating unique random number code 
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    matrix[i, j] = randNum.Next(1, 101);
+                }
+            }
+
+            return matrix;
+        }
+
+        public static void DisplayArray(int[,] matrix, int rows, int columns)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+
+
+        //SwapRow method
+        public static void SwapRow(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            for (int i = 0; i < rows / 2; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    int temp = matrix[i, j];
+                    matrix[i, j] = matrix[rows - 1 - i, j];
+                    matrix[rows - i - 1, j] = temp;
+                }
+            }
+
+        }
+
+
+
+
+        //SwapColumn method
+        public static void SwapColumn(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols / 2; j++)
+                {
+                    int temp = matrix[i, j];
+                    matrix[i, j] = matrix[i, cols - j - 1];
+                    matrix[i, cols - j - 1] = temp;
+                }
+            }
+        }
+
+
+
+
+        //RotateLeftDiagonal method
+        public static void RotateLeftDiagonal(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            if (rows != cols)
+            {
+                Console.WriteLine("Matrix is not square");
+                return;
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = i; j < cols; j++)
+                {
+                    int temp = matrix[i, j];
+                    matrix[i, j] = matrix[j, i];
+                    matrix[j, i] = temp;
+                }
+            }
+
+            // Swap the number of rows and columns
+            int tempSize = rows;
+            rows = cols;
+            cols = tempSize;
+        }
+
+
+
+        //RotateRightDiagonal
+        public static void RotateRightDiagonal(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            if (rows != cols)
+            {
+                Console.WriteLine("Matrix is not square");
+                return;
+            }
+
+            // Exchange elements along the right diagonal
+            for (int i = 0; i < rows / 2; i++)
+            {
+                int j = cols - i - 1;
+                int temp = matrix[i, i];
+                matrix[i, i] = matrix[j, j];
+                matrix[j, j] = temp;
+            }
+
+            // Swap the number of rows and columns
+            int tempSize = rows;
+            rows = cols;
+            cols = tempSize;
+        }
+
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("A3-TogzhanTolegen")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("A3-TogzhanTolegen")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("83875e04-2823-4255-bd76-da396f337c18")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
